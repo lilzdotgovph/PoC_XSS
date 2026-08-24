2d6b51544a2b8724119a3b90dabb56be66ef7e39
lilzdotgovph <108351739+lilzdotgovph@users.noreply.github.com>
2026-08-24 18:24:28 +0800

    Update simple_xss_with_img.js

--- simple_xss_with_img.js
+++ simple_xss_with_img.js
@@ -1,1 +1,1 @@
-const img=Object.assign(document.createElement("img"),{src:"https://github.com/lilzdotgovph/PoC_XSS/blob/main/KPMG.png",width:100,height:50});document.body.appendChild(img);
+const img=Object.assign(document.createElement("img"),{src:"https://github.com/lilzdotgovph/PoC_XSS/blob/main/KPMG.png",width:100,height:50});document.body.appendChild(img);alert("PoC XSS with minimal image");
